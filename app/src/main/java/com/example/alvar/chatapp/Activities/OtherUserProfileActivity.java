842444167a8e78ba49399dcc0caef852f3cfca8d
Alvaro Isea <alvaroe9448@gmail.com>
2018-12-27 14:17:12 +0100

    user info retrieved successfully from db into other users page

--- app/src/main/java/com/example/alvar/chatapp/Activities/OtherUserProfileActivity.java
+++ app/src/main/java/com/example/alvar/chatapp/Activities/OtherUserProfileActivity.java
@@ -1,5 +1,6 @@
 package com.example.alvar.chatapp.Activities;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,12 +9,21 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.alvar.chatapp.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class OtherUserProfileActivity extends AppCompatActivity {
     //log
     private static final String TAG = "OtherUserProfilePage";
+    //firebase
+    private FirebaseDatabase database;
+    private DatabaseReference dbRef;
     //ui elements
     private CircleImageView otherUserImg;
     private TextView usernameOtherUser, statusOtherUser;
@@ -27,10 +37,10 @@ public class OtherUserProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_other_user_profile);
 
 
-
+        initFirebase();
         receiveUserId();
+        retrieveInfo();
         binUI();
-
 
 
     }
@@ -42,6 +52,12 @@ public class OtherUserProfileActivity extends AppCompatActivity {
         btnOther = findViewById(R.id.buttonOtherUser);
     }
 
+    private void initFirebase(){
+        database = FirebaseDatabase.getInstance();
+        //we aim to "Users" node
+        dbRef = database.getReference().child("Users");
+    }
+
     /**
      * in this method we receive the unique user Id given by firebase to any user.
      * @return
@@ -51,4 +67,48 @@ public class OtherUserProfileActivity extends AppCompatActivity {
         Log.i(TAG, "receiveUserId: user ID: " + otherUserIdReceived);
         return otherUserIdReceived;
     }
+
+    /**
+     * here in this method is th logic to fetch info from the databse
+     */
+    private void retrieveInfo(){
+
+        dbRef.child(receiveUserId()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                setInfo(dataSnapshot);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
+    /**
+     * this method is in charge of setting the info fetched from the db into the UI
+     * @param dataSnapshot
+     */
+    private void setInfo(DataSnapshot dataSnapshot) {
+
+        String username = dataSnapshot.child("name").getValue().toString();
+        String status = dataSnapshot.child("status").getValue().toString();
+        String image = dataSnapshot.child("image").getValue().toString();
+
+        usernameOtherUser.setText(username);
+        statusOtherUser.setText(status);
+        if (image.equals("image")){
+            otherUserImg.setImageResource(R.drawable.imgdefault);
+        } else{
+            //here we set image from database into imageView
+            Picasso.get().load(image).into(otherUserImg);
+
+        }
+
+
+    }
+
 }
