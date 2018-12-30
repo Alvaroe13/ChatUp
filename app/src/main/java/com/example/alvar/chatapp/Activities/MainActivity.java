f531357bda9881815b02112065365118ac069c74
Alvaro Isea <alvaroe9448@gmail.com>
2018-12-30 02:51:03 +0100

    added image and username to toolbar

--- app/src/main/java/com/example/alvar/chatapp/Activities/MainActivity.java
+++ app/src/main/java/com/example/alvar/chatapp/Activities/MainActivity.java
@@ -13,20 +13,24 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.widget.EditText;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.example.alvar.chatapp.Adapter.ViewPagerAdapter;
 import com.example.alvar.chatapp.Fragments.ChatsFragment;
 import com.example.alvar.chatapp.Fragments.ContactsFragment;
 import com.example.alvar.chatapp.Fragments.GroupsFragment;
 import com.example.alvar.chatapp.R;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,13 +38,16 @@ public class MainActivity extends AppCompatActivity {
 
     //Firebase
     private FirebaseAuth mAuth;
-    private FirebaseUser currentUser;
     private FirebaseDatabase database;
     private DatabaseReference dbRef;
     //UI elements
     private Toolbar toolbarMain;
     private ViewPager viewPager;
     private TabLayout tabLayout;
+    private CircleImageView imageToolbarMain;
+    private TextView usernameToolbarMain;
+    //vars
+    private String currentUserID;
 
 
     @Override
@@ -52,9 +59,13 @@ public class MainActivity extends AppCompatActivity {
         //init UI elements
         bindUI();
         //init toolbar and set title
-        setToolbar(getString(R.string.app_name), false);
+        setToolbar("", false);
         //init firebase
         initFirebase();
+        //set image and username from db to toolbar
+        fetchInfoFromDb();
+        //when toolbar is clicked by the user
+        toolbarOnClick();
         // viewPagerAdapter init
         initPageAdapter(viewPager);
         tabLayout.setupWithViewPager(viewPager);
@@ -68,6 +79,9 @@ public class MainActivity extends AppCompatActivity {
         viewPager = findViewById(R.id.viewPager);
         tabLayout = findViewById(R.id.tabLayout);
         viewPager = findViewById(R.id.viewPager);
+        imageToolbarMain = findViewById(R.id.imageToolbar);
+        usernameToolbarMain = findViewById(R.id.usernameMainToolbar);
+
     }
 
     /**
@@ -127,7 +141,7 @@ public class MainActivity extends AppCompatActivity {
         //database init
         database = FirebaseDatabase.getInstance();
         //database ref init
-        dbRef = database.getReference();
+        dbRef = database.getReference().child("Users");
     }
 
 
@@ -173,6 +187,12 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intentAllUsers);
     }
 
+    /**
+     * this method contains the pop up message when user clicks log out from menu option
+     * @param title
+     * @param message
+     * @return
+     */
     private AlertDialog alertMessage(String title, String message){
 
 
@@ -192,6 +212,61 @@ public class MainActivity extends AppCompatActivity {
                 .show();
 
         return popUpWindow;
+    }
+
+    /**
+     * this method is in charge of fetching info from the db and set it into the toolbar
+     */
+    private void fetchInfoFromDb(){
+
+        currentUserID = mAuth.getCurrentUser().getUid();
+
+        dbRef.child(currentUserID).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                if (dataSnapshot.exists()){
+
+                    String imageThumbnailToolbar = dataSnapshot.child("imageThumbnail").getValue().toString();
+                    String usernameToolbar = dataSnapshot.child("name").getValue().toString();
+
+                    Log.i(TAG, "onDataChange: username set");
+                    usernameToolbarMain.setText(usernameToolbar);
+
+                    if (imageThumbnailToolbar.equals("imgThumbnail")){
+                        imageToolbarMain.setImageResource(R.drawable.profile_image);
+                    }else{
+                        Log.i(TAG, "onDataChange: image set");
+                        Glide.with(MainActivity.this).load(imageThumbnailToolbar).into(imageToolbarMain);
+                    }
+
+                } else{
+                    Toast.makeText(MainActivity.this, "Something went wrong with your network", Toast.LENGTH_SHORT).show();                    
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+
+            }
+        });
+
+
+    }
+
+    private void toolbarOnClick(){
+        toolbarMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToProfile();
+            }
+        });
+    }
+
+    private void goToProfile() {
+        Intent intentGoToProfile = new Intent(MainActivity.this, SettingsActivity.class);
+        startActivity(intentGoToProfile);
     }
 
 
