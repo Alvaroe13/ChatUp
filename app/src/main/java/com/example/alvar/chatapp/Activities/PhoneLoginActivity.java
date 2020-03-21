250198fee1cd06968afa2375e46fee15d41f45f7
Alvaro Isea <alvaroe9448@gmail.com>
2020-03-21 01:40:59 +0100

    Small changes in phone login activity and login layout

--- app/src/main/java/com/example/alvar/chatapp/Activities/PhoneLoginActivity.java
+++ app/src/main/java/com/example/alvar/chatapp/Activities/PhoneLoginActivity.java
@@ -1,19 +1,27 @@
 package com.example.alvar.chatapp.Activities;
 
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.TextInputEditText;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.alvar.chatapp.R;
+import com.example.alvar.chatapp.Utils.SnackbarHelper;
 
 public class PhoneLoginActivity extends AppCompatActivity {
 
-    private TextInputLayout textPhoneNumber, textCode;
+    private static final String TAG = "PhoneLoginActivity";
+
+    private EditText textPhoneNumber, textCode;
     private Button btnVerifyCode, btnSendCode;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,14 +56,25 @@ public class PhoneLoginActivity extends AppCompatActivity {
         btnSendCode.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //when send code button is pressed let's remove these fields
-                textPhoneNumber.setVisibility(View.INVISIBLE);
-                btnSendCode.setVisibility(View.INVISIBLE);
-                //and let's show these fields
-                textCode.setVisibility(View.VISIBLE);
-                btnVerifyCode.setVisibility(View.VISIBLE);
+
+                //we get the phone number and store it in a variable
+                String phoneNumber = textPhoneNumber.getText().toString();
+
+                if (TextUtils.isEmpty(phoneNumber)){
+                    Log.i(TAG, "onClick: field empty");
+                    Toast.makeText(PhoneLoginActivity.this, "You must insert a phone number", Toast.LENGTH_SHORT).show();
+
+                } else{
+                    //when send code button is pressed let's remove these fields
+                    textPhoneNumber.setVisibility(View.INVISIBLE);
+                    btnSendCode.setVisibility(View.INVISIBLE);
+                    //and let's show these fields
+                    textCode.setVisibility(View.VISIBLE);
+                    btnVerifyCode.setVisibility(View.VISIBLE);
+                }
             }
         });
+
     }
 
     /**
@@ -63,12 +82,9 @@ public class PhoneLoginActivity extends AppCompatActivity {
      */
     private void loginButton() {
 
-
-
         btnVerifyCode.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                     Toast.makeText(PhoneLoginActivity.this, "To be continued...", Toast.LENGTH_SHORT).show();
             }
         });
