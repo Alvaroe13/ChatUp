f5ef7ab2d34c29af922a56a7e766406b0581b65b
Alvaro Isea <alvaroe9448@gmail.com>
2020-05-01 21:21:29 +0200

    *open image room when user pressed over Image Dialog

--- app/src/main/java/com/example/alvar/chatapp/Dialogs/ImageProfileShow.java
+++ app/src/main/java/com/example/alvar/chatapp/Dialogs/ImageProfileShow.java
@@ -2,6 +2,7 @@ package com.example.alvar.chatapp.Dialogs;
 
 
 import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,6 +14,7 @@ import android.view.View;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+import com.example.alvar.chatapp.Activities.ImageActivity;
 import com.example.alvar.chatapp.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -36,8 +38,13 @@ public class ImageProfileShow extends DialogFragment {
     private FirebaseAuth mAuth;
     private FirebaseDatabase database;
     private DatabaseReference dbUsersRef;
+    //ui
+    private View imageProfileView;
+    private LayoutInflater viewInflater;
+    private AlertDialog.Builder builder;
+    private ImageView imageProfileDialog;
     //vars
-    private String currentUserID;
+    private String currentUserID, image;
 
 
     @NonNull
@@ -47,46 +54,7 @@ public class ImageProfileShow extends DialogFragment {
         //init firebase method
         initFirebase();
 
-        //Create dialog builder
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        LayoutInflater viewInflater= getActivity().getLayoutInflater();
-        View imageProfileView = viewInflater.inflate(R.layout.profile_dialog, null);
-        final ImageView imageProfileDialog = imageProfileView.findViewById(R.id.imageProfileDialog);
-        builder.setView(imageProfileView);
-
-
-        //down here we fetch the info (image) from the db to be shown later on the Alert dialog
-        dbUsersRef.child(currentUserID).addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-
-                //we store the image from the db into String var
-                String image = dataSnapshot.child("image").getValue().toString();
-
-                if (getActivity() == null) {
-
-                    Log.i(TAG, "onDataChange: error with context: ");
-                     return ;
-                }
-
-                //set image from firebase databsae to UI
-                if ( image.equals("image")){
-                    imageProfileDialog.setImageResource(R.drawable.profile_image);
-                }else{
-                    Glide.with(getActivity()).load(image).into(imageProfileDialog);
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
-
-
-        return builder.create();
-
-
+        return showAlertDialog();
     }
 
     /**
@@ -103,6 +71,80 @@ public class ImageProfileShow extends DialogFragment {
         currentUserID = mAuth.getCurrentUser().getUid();
     }
 
+    /**
+     * this method inflates the Alert Dialog layout
+     * @return
+     */
+    private  AlertDialog showAlertDialog() {
+
+        //Create dialog builder
+        builder = new AlertDialog.Builder(getActivity());
+        viewInflater= getActivity().getLayoutInflater();
+        imageProfileView = viewInflater.inflate(R.layout.profile_dialog, null);
+        imageProfileDialog = imageProfileView.findViewById(R.id.imageProfileDialog);
+        builder.setView(imageProfileView);
+        //lets fetch info from db
+        fetchInfoFromDB(imageProfileDialog);
+
+        return builder.create();
+    }
+
+    /**
+     * method is in charge of sending the photo information to the Image room if photo pressed
+     */
+    private void imagePressed() {
+        imageProfileView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //note that we use "messageContent" as id like in the rest of the app
+                Intent i = new Intent(getActivity(), ImageActivity.class);
+                i.putExtra("messageContent", image);
+                startActivity(i);
+            }
+        });
+    }
+
+    /**
+     * this one fetches the info from db
+     * @param imageProfileDialog
+     */
+    private void fetchInfoFromDB(final ImageView imageProfileDialog) {
+
+        //down here we fetch the info (image) from the db to be shown later on the Alert dialog
+        dbUsersRef.child(currentUserID).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                if (dataSnapshot.exists()){
+
+                    //we store the image from the db into String var
+                    image = dataSnapshot.child("image").getValue().toString();
+
+                    if (getActivity() == null) {
+                        Log.i(TAG, "onDataChange: error with context: ");
+                        return ;
+                    }
+
+                    //set image from firebase database to UI
+                    if ( image.equals("image")){
+                        imageProfileDialog.setImageResource(R.drawable.profile_image);
+                    }
+                    else{
+                        Glide.with(getActivity()).load(image).into(imageProfileDialog);
+                    }
+
+                    imagePressed();
+
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
 
 
 }
