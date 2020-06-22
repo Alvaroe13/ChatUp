4cce34d369aff33cf7fe9c1f1be3a944959a6566
Alvaro Isea <alvaroe9448@gmail.com>
2020-06-22 13:18:26 +0200

    *show last message in chatFragment

--- app/src/main/java/com/example/alvar/chatapp/Adapter/ChatsAdapter.java
+++ app/src/main/java/com/example/alvar/chatapp/Adapter/ChatsAdapter.java
@@ -12,8 +12,11 @@ import android.widget.TextView;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
 import com.example.alvar.chatapp.Activities.ChatActivity;
+import com.example.alvar.chatapp.Model.Messages;
 import com.example.alvar.chatapp.Model.User;
 import com.example.alvar.chatapp.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -47,7 +50,6 @@ public class ChatsAdapter extends RecyclerView.Adapter<ChatsAdapter.ChatsViewHol
     @Override
     public ChatsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(context).inflate(R.layout.chats_individual_layout, parent, false);
-
 
 
         return new ChatsAdapter.ChatsViewHolder(view);
@@ -133,19 +135,110 @@ public class ChatsAdapter extends RecyclerView.Adapter<ChatsAdapter.ChatsViewHol
      */
     private void typingState(String typingState, String contactID, ChatsViewHolder holder) {
 
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        String currentUserID = auth.getCurrentUser().getUid();
+
+
+
         if (typingState.equals("yes")) {
             try {
                 holder.smallIcon.setVisibility(View.GONE);
-                holder.lastMessage.setText(R.string.typing);
-                holder.lastMessage.setTextColor(context.getResources().getColor(R.color.color_green));
+                holder.lastMessageField.setText(R.string.typing);
+                holder.lastMessageField.setTextColor(context.getResources().getColor(R.color.color_green));
             }
             catch (Exception e){
                 Log.i(TAG, "onDataChange: error: " + e.getMessage() );
+            }
+        }   else {
+            //this method show last message in the fragment list with conversations started
+            try {
+                showLastMessage(currentUserID, contactID, holder);
+
+                //holder.lastMessage, holder.lastMessageDateField, holder.smallIcon
+
+                holder.lastMessageField.setTextColor(context.getResources().getColor(R.color.color_grey));
+            }
+            catch (Exception ex){
+                Log.i(TAG, "onDataChange: error: " + ex.getMessage());
             }
         }
 
 
     }
+
+    /**
+     * This method is the one in charge of showing the last message in a conversation
+     *
+     * @param currentUserID
+     * @param otherUserID
+     */
+    private void showLastMessage(final String currentUserID, final String otherUserID, final  ChatsViewHolder holder) {
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+        DatabaseReference dbChatsNodeRef = database.getReference().child("Chats").child("Messages");
+
+        //retrieve info from the db node "Chats" / "Messages"
+        dbChatsNodeRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+
+                    if (dataSnapshot.exists()) {
+
+                        //we bind the Message node in firebase database with the JAVA model "Messages"
+                        Messages message = snapshot.getValue(Messages.class);
+
+                        Log.i(TAG, "onDataChange: sender: " + message.getSenderID());
+                        Log.i(TAG, "onDataChange: receiver: " + message.getReceiverID());
+
+                        if (message.getSenderID().equals(currentUserID) && message.getReceiverID().equals(otherUserID) ||
+                                message.getSenderID().equals(otherUserID) && message.getReceiverID().equals(currentUserID)) {
+
+                           holder.smallIcon.setVisibility(View.GONE);
+
+                            switch (message.getType()) {
+                                case "image":
+                                    holder.smallIcon.setVisibility(View.VISIBLE);
+                                    holder.lastMessageField.setText(R.string.photo);
+                                    break;
+                                case "pdf":
+                                    holder.smallIcon.setVisibility(View.VISIBLE);
+                                    holder.smallIcon.setBackgroundResource(R.drawable.file_icon);
+                                    holder.lastMessageField.setText(R.string.PDF);
+                                    break;
+                                case "docx":
+                                    holder.smallIcon.setVisibility(View.VISIBLE);
+                                    holder.smallIcon.setBackgroundResource(R.drawable.file_icon);
+                                    holder.lastMessageField.setText(R.string.Word_Document);
+                                    break;
+                                default: //"text" is the one by default
+                                    String lastMessage = message.getMessage();
+                                    holder.lastMessageField.setText(lastMessage);
+
+                                    String lastMessageDate = message.getMessageDate();
+                                    holder.lastMessageDateField.setText(lastMessageDate);
+                            }
+
+
+                        }
+
+                    }
+
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+    }
+
 
     /**
      * method in charge of showing green dot if user is online
@@ -191,7 +284,7 @@ public class ChatsAdapter extends RecyclerView.Adapter<ChatsAdapter.ChatsViewHol
 
         CardView chatLayout;
         CircleImageView chatImageContact, onlineIcon;
-        TextView username, lastMessage, lastMessageDateField;
+        TextView username, lastMessageField, lastMessageDateField;
         ImageButton smallIcon;
 
 
@@ -200,7 +293,7 @@ public class ChatsAdapter extends RecyclerView.Adapter<ChatsAdapter.ChatsViewHol
             chatLayout = layout.findViewById(R.id.cardViewAllUsers);
             chatImageContact = layout.findViewById(R.id.imageChat);
             username = layout.findViewById(R.id.usernameChat);
-            lastMessage = layout.findViewById(R.id.lastMessage);
+            lastMessageField = layout.findViewById(R.id.lastMessage);
             lastMessageDateField = layout.findViewById(R.id.lastMessageDate);
             onlineIcon = layout.findViewById(R.id.onlineIcon);
             smallIcon = layout.findViewById(R.id.smallIcon);
